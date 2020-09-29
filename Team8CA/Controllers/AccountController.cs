a41ec859a90b0441914b512364466ddcb14fb500
Sean Ooi <69132873+seano188@users.noreply.github.com>
2020-09-29 20:41:10 +0800

    Added new controller

---
+++ Team8CA/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Team8CA.Models;
+
+namespace Team8CA.Controllers
+{
+    public class AccountController : Controller
+    {
+                
+        //GET : Account
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        void Controller()
+        {
+
+        }
+
+
+
+        public IActionResult Verify(Account acc)
+        {
+            return View();
+        }
+    }
+}
