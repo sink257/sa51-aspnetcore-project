c3bb43cba805626f93a89e1be8ff36811dc2258b
Jia Xin Lee <e0641552@u.nus.edu>
2020-10-01 12:18:57 +0800

    hi

--- Team8CA/Controllers/GalleryController.cs
+++ Team8CA/Controllers/GalleryController.cs
@@ -10,7 +10,22 @@ namespace Team8CA.Controllers
     {
         public IActionResult Index()
         {
+            string[] imgs = {
+                "photo-1593642632559-0c6d3fc62b89",
+                "photo-1497366754035-f200968a6e72",
+                "photo-1497366811353-6870744d04b2",
+                "photo-1524758631624-e2822e304c36",
+                "photo-1531973576160-7125cd663d86",
+                "photo-1505409859467-3a796fd5798e",
+                "photo-1564069114553-7215e1ff1890"
+            };
+
+            ViewData["images"] = imgs;
+            ViewData["url_prefix"] = "https://images.unsplash.com/";
+            ViewData["url_postfix"] = "?w=350";
+
             return View();
         }
+
     }
 }
