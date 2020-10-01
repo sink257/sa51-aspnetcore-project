0cc8079c735cdb85ec67ca2a2cc2521c00be99a1
Sankalp <sink257@hotmail.com>
2020-10-01 12:11:52 +0800

    Search Implementation

---
+++ Team8CA/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Team8CA.Models;
+
+namespace Team8CA.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(string keywords)
+        {
+            string[] products = { "product1", "product2", "product3", "product4" };
+            
+            int x;
+
+            bool result = int.TryParse(keywords, out x);
+
+            var product = (from p in products
+                           where p == keywords
+                           select p).ToList();
+
+            return View(product);
+        }
+    }
+}
---
+++ Team8CA/Models/Search.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team8CA.Models
+{
+    public class Search
+    {
+        public string product {get;set;}
+    }
+}
