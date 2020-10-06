6535c70de1afda37e4b6e831aa5ac9bfffc65dc2
Huaqin <53359929+Uibany@users.noreply.github.com>
2020-10-06 15:31:48 +0800

    Modified gallery layout

--- Team8CA/Startup.cs
+++ Team8CA/Startup.cs
@@ -45,7 +45,7 @@ namespace Team8CA
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            db.Database.EnsureDeleted();
+            //db.Database.EnsureDeleted();
             db.Database.EnsureCreated();
 
             app.UseStaticFiles();
