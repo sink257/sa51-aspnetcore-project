b1c4316b07a3f6ec3e6491d1942633592797a8ae
Sankalp <sink257@hotmail.com>
2020-10-06 12:14:37 +0800

    Resolved conflict

--- Team8CA/Controllers/GalleryController.cs
+++ Team8CA/Controllers/GalleryController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PagedList;
 using Team8CA.Models;
+//using Team8CA.Services;
 
 namespace Team8CA.Controllers
 {
@@ -30,6 +32,11 @@ namespace Team8CA.Controllers
             return View();
         }
 
+        //public GalleryController(int ID, string ProductName, double ProductPrice, bool ProductAvailability, string ProductDescription)
+        //{ 
+            
+        //}
+
         public IActionResult AntivirusAndSecurity()
         {
             return View();
@@ -43,11 +50,32 @@ namespace Team8CA.Controllers
 
         }
 
-
-
         public IActionResult DesignAndIllustration()
         {
             return View();
         }
+
+        //public IActionResult AddToCart([FromServices] CartRelatedService srv, int prdId)
+        //{
+        //    var customerId = HttpContext.Session.GetInt32("customerId") ?? 0;
+        //    //if (customerId == 0)
+        //    //{
+        //    //    AddToCartForSession(srv, prdId, 1);
+        //    //}
+        //    //else
+        //    //{
+        //        ViewData["ItemCount"] = srv.AddProductsToCart(customerId, prdId, 1);
+        //    //}
+        //    return PartialView("_CartIcon");
+        //}
+
+
+
+
+
+
+
+
+
     }
 }
