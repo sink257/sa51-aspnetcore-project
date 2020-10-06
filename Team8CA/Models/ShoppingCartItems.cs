899a667d63e16ad3f5b67ecc0d52c4a4eade10db
Sean Ooi <69132873+seano188@users.noreply.github.com>
2020-10-06 13:14:12 +0800

    Updates to shopping cart

--- Team8CA/Models/ShoppingCartItems.cs
+++ Team8CA/Models/ShoppingCartItems.cs
@@ -1,34 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace Team8CA.Models
-//{
-//    public class ShoppingCartItems
-//    {
-//        public int Id { get; set; } //individual product ID
+namespace Team8CA.Models
+{
+    public class ShoppingCartItems
+    {
+        public int Id { get; set; } //individual product ID
 
-//        public int ShoppingcartId { get; set; }
+        public int ShoppingcartId { get; set; }
 
-//        public int ProductId { get; set; }
+        public int ProductId { get; set; }
 
-//        public int QuantityPerItem { get; set; }
+        public int QuantityPerItem { get; set; }
 
-//        public virtual Product Products { get; set; }
-//        public virtual ShoppingCart ShoppingCart { get; set; }
+        public virtual Products Products { get; set; }
+        public virtual ShoppingCart ShoppingCart { get; set; }
 
-//        public ShoppingCartItems(int ShoppingCartId, int productId)
-//        {
-//            ShoppingcartId = ShoppingCartId;
-//            ProductId = productId;
-//            QuantityPerItem = 0;
-//        }
-
-
-
-
-
-
-//    }
-//}
+        public ShoppingCartItems(int ShoppingCartId, int productId)
+        {
+            ShoppingcartId = ShoppingCartId;
+            ProductId = productId;
+            QuantityPerItem = 0;
+        }
+    }
+}
