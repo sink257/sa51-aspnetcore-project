f93a908cc0205301ce02b2d88c68142bd8fc064a
Sean Ooi <69132873+seano188@users.noreply.github.com>
2020-10-09 11:52:10 +0800

    Update Shopping Cart Logic

--- Team8CA/DataAccess/AppDbContext.cs
+++ Team8CA/DataAccess/AppDbContext.cs
@@ -28,8 +28,8 @@ namespace Team8CA.DataAccess
         //public DbSet<Products> Products { get; set; }
         //implementation of Model methods
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<ShoppingCart> ShoppingCart { get; set; }
         public DbSet<ShoppingCartItem> ShoppingCartItem { get; set; }
+        public DbSet<ShoppingCart> ShoppingCart { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<Products> Products { get; set; }
         public DbSet<Session> Sessions { get; set; }
