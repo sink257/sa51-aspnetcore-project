3ededca3e880de6ecb5954bc2296481ed1fddb7d
Sankalp <sink257@hotmail.com>
2020-10-11 03:34:12 +0800

    Small fix

--- Team8CA/DataAccess/AppDbContext.cs
+++ Team8CA/DataAccess/AppDbContext.cs
@@ -13,34 +13,24 @@ namespace Team8CA.DataAccess
     public class AppDbContext : DbContext
     {
         protected IConfiguration configuration;
-        
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
 
-        //protected override void OnModelCreating(ModelBuilder model)
-        //{
-        //}
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             //modelBuilder.Entity<Review>().HasOne<Products>(p => p.Products).WithMany(p => p.Reviews);
             //modelBuilder.Entity<ShoppingCart>().HasKey(x => new { x.ShoppingCartId, x.CustomerId });
-
-
         }
 
         //implementation of Model methods
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<ShoppingCartItem> ShoppingCartItem { get; set; }
-        public DbSet<ShoppingCart> ShoppingCart { get; set; }
-        public DbSet<Review> Reviews { get; set; }
-        public DbSet<Products> Products { get; set; }
         public DbSet<Session> Sessions { get; set; }
-
-
-
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<ShoppingCart> ShoppingCart { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItem { get; set; }
     }
 }
