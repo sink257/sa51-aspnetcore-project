ab5c1ee0d85e5eb3a5e651c3b8a1582e5b7b37d7
Sean Ooi <69132873+seano188@users.noreply.github.com>
2020-10-12 15:58:37 +0800

    COMPLETED CART MERGE UPON LOG IN!!

--- Team8CA/Controllers/OrderHistoryController.cs
+++ Team8CA/Controllers/OrderHistoryController.cs
@@ -48,6 +48,25 @@ namespace Team8CA.Controllers
         }
         public IActionResult RecentOrder(int orderId)
         {
+            string customerId = Request.Cookies["customerId"];
+            List<Order> orders = db.Order.Where(o => o.CustomerId == customerId).ToList();
+            ViewData["order"] = orders;
+
+            ViewData["firstname"] = Request.Cookies["firstname"];
+            string sessionid = Request.Cookies["sessionId"];
+            ViewData["sessionId"] = sessionid;
+
+            ViewData["customerid"] = customerId;
+            List<ShoppingCartItem> shoppingcart = db.ShoppingCartItem.Where(x => x.ShoppingCartId == customerId).ToList();
+            List<ShoppingCartItem> shoppingcartNull = db.ShoppingCartItem.Where(x => x.ShoppingCartId == "0").ToList();
+            if (sessionid != null)
+            {
+                ViewData["cartcount"] = shoppingcart.Count;
+            }
+            else
+            {
+                ViewData["cartcount"] = shoppingcartNull.Count;
+            }
             List<OrderDetails> orderdetail = db.OrderDetails.Where(o => o.OrderId == orderId).ToList();
             ViewData["orderdetail"] = orderdetail;
             return View();
